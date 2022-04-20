5f976e67263712117068c6f4181fec0471fadefb
kristaniguchi <kristinetq@sccwrp.org>
2022-04-20 14:04:34 -0700

    demo update

--- 02_FFC_API_Example_Usage.R
+++ 02_FFC_API_Example_Usage.R
@@ -11,60 +11,39 @@ library(ffcAPIClient)
 ffctoken <-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdE5hbWUiOiJLcmlzIiwibGFzdE5hbWUiOiJUYW5pZ3VjaGkgUXVhbiIsImVtYWlsIjoia3Jpc3RpbmV0cUBzY2N3cnAub3JnIiwicm9sZSI6IlVTRVIiLCJpYXQiOjE2MTMwNjkxMDJ9.pOy0lTuKBm75Us_PtD8G2yD_l9ISZea2atsPwWUCsDw'
 
 ###########################################
-### 1. FFC for a single USGS Gage
-
-# start ffc processor
-ffc <- FFCProcessor$new()
-
-# set up gage id and token
-ffc$set_up(gage_id = 11427000,
-           token = ffctoken)  
-           # OPTIONAL: indicate NHD COMID, set up the plot output folder
-           #comid = segment_comid,
-           #plot_output_folder = "./data/Output_02/")
-
-#run calculator
-ffc$run()
-
-#get FFC results - annual metrics
-ffc$ffc_results
-#save as a csv
-write.csv(ffc$ffc_results, file="./data/Output_02/ffc_annual_results.csv", row.names = TRUE)
-
-#get FFC percentiles - p10-p90 for every metric
-ffc$ffc_percentiles
-#save as a csv
-write.csv(ffc$ffc_percentiles, file="./data/Output_02/ffc_percentiles_observed.csv", row.names = TRUE)
-
-#get predicted reference percentiles
-ffc$predicted_percentiles
-#save as a csv
-write.csv(ffc$predicted_percentiles, file="./data/Output_02/ffc_percentiles_reference.csv", row.names = TRUE)
-
-#get alteration
-ffc$alteration
-#save as a csv
-write.csv(ffc$alteration, file="./data/Output_02/ffc_alteration.csv", row.names = TRUE)
-
-###########################################
-#### 2. Save results in a single folder
-
-#run evaluate gage alteration to calculate FFM, pull predicted natural FFM, 
+#Calculate observed FFM, 
+  #pull predicted natural FFM, 
   #and conduct alteration assessment
 
-#evaluate alteration function
-alteration <- evaluate_gage_alteration(gage_id = 11427000, 
-                                       token = ffctoken, 
-                                       force_comid_lookup = TRUE,
-                                       plot_output_folder = "./data/Output_02/")
-#get alteration data
-alteration$alteration
-#save as a csv
-write.csv(ffc$ffc_percentiles, file="./data/Output_02/ffc_percentiles_observed.csv", row.names = TRUE)
+##Option 1: if your observed data is from a USGS gage, use "evaluate gage alteration" function.
+
+#Information needed to run function
+#USGS gage: 11445500, SF American River near Lotus
+#COMID: 14982092 
+
+#run evaluate gage alteration function
+SFA_observed <- ffcAPIClient::evaluate_gage_alteration(gage_id = 11445500, 
+                                         token = ffctoken, 
+                                         comid = 14982092,
+                                         plot_output_folder = "./data/Output_02/")
+
+#create data frames to view the output 
+SFA_pred_percentiles <- SFA_observed$predicted_percentiles
+SFA_pred_WYT_percentiles <- SFA_observed$predicted_wyt_percentiles
+SFA_obs_percentiles <- SFA_observed$ffc_percentiles
+SFA_ffc_results <- SFA_observed$ffc_results
+
+SFA_doh_data <- SFA_observed$doh_data
+SFA_alteration <- SFA_observed$alteration
+
+#save dataframes as a csv
+write.csv(SFA_alteration, file="./data/Output_02/SFA_alteration.csv", row.names = FALSE)
 
 
 ###########################################
-### 3. FFC with your own flow data
+### Option 2. FFC with your own flow data
+
+#What you need: COMID (14996611) and flow timeseries
 
 #read in your own flow dataframe with date column and flow column
 flow.data <- read.csv("./data/Input_02/sample.csv")
@@ -74,42 +53,24 @@ names(flow.data)
 #if column headers are incorrect, rename to date and flow
 names(flow.data) <- c("date", "flow")
 
-#format date to YYYY-mm-dd
-flow.data$date <- as.POSIXct(flow.data$date, format = "%m/%d/%Y")
-#format date as class
-flow.data$date <- format(flow.data$date, "%Y-%m-%d")
-#format flow as numeric
-flow.data$flow <- as.numeric(flow.data$flow)
+#run the evaluate alteration function
+Sample_observed <- ffcAPIClient::evaluate_alteration(timeseries_df = flow.data, 
+                                                     token = ffctoken, 
+                                                     plot_output_folder = "./data/Output_02/Sample/", 
+                                                     comid=14996611 )
 
-# start ffc processor
-ffc <- FFCProcessor$new()
 
-# set up flow timeseries, token, and comid
-ffc$set_up(timeseries = flow.data,
-           token = ffctoken,
-           comid = 14996611)  
+#create data frames to view the output
+Sample_pred_percentiles <- Sample_observed$predicted_percentiles
+Sample_pred_WYT_percentiles <- Sample_observed$predicted_wyt_percentiles
+Sample_obs_percentiles <- Sample_observed$ffc_percentiles
+Sample_ffc_results <- Sample_observed$ffc_results
+Sample_doh_data <- Sample_observed$doh_data
+Sample_alteration <- Sample_observed$alteration
 
-# run calculator
-ffc$run()
 
-#get FFC results - annual metrics
-ffc$ffc_results
 #save as a csv
-write.csv(ffc$ffc_results, file="./data/Output_02/ffc_annual_results.csv", row.names = TRUE)
+write.csv(Sample_alteration, file="./data/Output_02/Sample/Sample_alteration.csv", row.names = FALSE)
 
-#get FFC percentiles - p10-p90 for every metric
-ffc$ffc_percentiles
-#save as a csv
-write.csv(ffc$ffc_percentiles, file="./data/Output_02/ffc_percentiles_observed.csv", row.names = TRUE)
-
-#get predicted reference percentiles
-ffc$predicted_percentiles
-#save as a csv
-write.csv(ffc$predicted_percentiles, file="./data/Output_02/ffc_percentiles_reference.csv", row.names = TRUE)
-
-#get alteration
-ffc$alteration
-#save as a csv
-write.csv(ffc$alteration, file="./data/Output_02/ffc_alteration.csv", row.names = TRUE)
 
 
